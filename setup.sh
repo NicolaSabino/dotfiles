9bc848f2515fac9c3dbb1c9dbf1b0d2810b16717
Nicola Sabino <nicola.sabino94@gmail.com>
2022-12-03 20:48:38 +0100

    [script] New folder name for config files

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ test=1 # set to 1 to run in test mode
 
 target_dev_name='dev'
 target_dot_name='.dotfiles'
+script_home=$HOME
 
 # Suitable matching patterns for dev target
 # - Dev
@@ -16,29 +17,28 @@ target_dot_name='.dotfiles'
 # - development
 dev_regex='.*/[D|d]ev\(el\)*\(op\)*\(ment\)*$'
 
-home=$HOME
 
 if [ test ]
 then
     echo "Using test environment"
     if [ ! -d test ] ; then mkdir test ; fi
-    $HOME=test
+    script_home='test'
 fi
 
-# Find the `dev` directory in $home
-dev_dir_path=`find $home -type d -regex "${dev_regex}" -print -quit`
+# Find the `dev` directory in $script_home
+dev_dir_path=`find $script_home -type d -regex "${dev_regex}" -print -quit`
 
 # Find the `.dotfiles` folder
-dot_dir_path=`find $home -maxdepth 1 -type d -name ${target_dot_name}`
+dot_dir_path=`find $script_home -maxdepth 1 -type d -name ${target_dot_name}`
 
-# If `dev` folder does not exists in $home create it
+# If `dev` folder does not exists in $script_home create it
 if [ "$dev_dir_path" != "" ]
 then
     echo "Found dev enviromnet in ${dev_dir_path}"
 else
     dev_dir_path=$target_dev_name
     echo "Create new ${target_dev_name} directory"
-    mkdir $home/$target_dev_name
+    mkdir $script_home/$target_dev_name
 fi
 
 # Check if `.dotfiles` exists
@@ -47,16 +47,16 @@ then
     echo "Found dotfiles folder ${dot_dir_path}"
 else
     echo "Create new ${target_dot_name} directory"
-    cp  -r .dotfiles $home/$target_dot_name
+    cp  -r config_files $script_home/$target_dot_name
 fi
 
-# Sync .dotfiles and $home/.dotfiles folders
+# Sync .dotfiles and $script_home/.dotfiles folders
 # If a file already exists we create a 
 # backup copy with datetime
-for file in `find .dotfiles/ -maxdepth 1 -type f -name ".*"`
+for file in `find config_files -maxdepth 1 -type f -name ".*"`
 do
     if [ -f $dot_dir_path ]
     then
-    echo ciao
+        echo $file
     fi
 done
