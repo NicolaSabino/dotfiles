d4bbd2d1d65720c5c2b9abc0a0350c5d3eaa026f
Nicola Sabino <nicola.sabino94@gmail.com>
2022-12-04 11:45:57 +0100

    [script] Main sync logic
    The script is now able to detect the presence of dev and dotfiles
    target in the user home directory.
    
    The test can be run in normal and test mode.
    While running the script in test, setting to TRUE the proper
    parameter within the script, a test folder is created and target
    directories change.
    
    The next step could be to accept parameters in input
    - prompt an help message
    - guide the setup of work and personal envs
    - change the target directories
    - flag the test mode via parameter.
    
    We could also create a test.sh script to test the several working
    scenarios

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-test=1 # set to 1 to run in test mode
+alias TRUE=1
+alias FALSE=0
+test=TRUE # set to 1 to run in test mode
 
 target_dev_name='dev'
 target_dot_name='.dotfiles'
-script_home=$HOME
+home_target=$HOME
 
 # Suitable matching patterns for dev target
 # - Dev
@@ -20,43 +22,64 @@ dev_regex='.*/[D|d]ev\(el\)*\(op\)*\(ment\)*$'
 
 if [ test ]
 then
-    echo "Using test environment"
+    printf "Using test environment\n"
     if [ ! -d test ] ; then mkdir test ; fi
-    script_home='test'
+    home_target='test'
 fi
 
-# Find the `dev` directory in $script_home
-dev_dir_path=`find $script_home -type d -regex "${dev_regex}" -print -quit`
+# Find the `dev` directory in $home_target
+dev_dir_path=`find $home_target -type d -regex "${dev_regex}" -print -quit`
 
 # Find the `.dotfiles` folder
-dot_dir_path=`find $script_home -maxdepth 1 -type d -name ${target_dot_name}`
+dot_dir_path=`find $home_target -maxdepth 1 -type d -name ${target_dot_name}`
 
-# If `dev` folder does not exists in $script_home create it
+# If `dev` folder does not exists in $home_target create it
+printf "Searching for ${target_dev_name} target\n"
 if [ "$dev_dir_path" != "" ]
 then
-    echo "Found dev enviromnet in ${dev_dir_path}"
+    printf "\tFound dev enviromnet in ${dev_dir_path}\n"
 else
     dev_dir_path=$target_dev_name
-    echo "Create new ${target_dev_name} directory"
-    mkdir $script_home/$target_dev_name
+    printf "\tCreate new ${target_dev_name} directory\n"
+    mkdir $home_target/$target_dev_name
 fi
 
 # Check if `.dotfiles` exists
+dotfiles_exists=FALSE
+printf "Searching for ${target_dot_name} target\n"
 if [ "$dot_dir_path" != "" ]
 then 
-    echo "Found dotfiles folder ${dot_dir_path}"
+    printf "\tFound dotfiles folder ${dot_dir_path}\n"
+    dotfiles_exists=TRUE
 else
-    echo "Create new ${target_dot_name} directory"
-    cp  -r config_files $script_home/$target_dot_name
+    printf "\tCreate new ${target_dot_name} directory\n"
+    cp  -r config_files $home_target/$target_dot_name
 fi
 
-# Sync .dotfiles and $script_home/.dotfiles folders
-# If a file already exists we create a 
+# Sync .dotfiles and $home_target/.dotfiles folders
+# If a file already exists we create a echo $dot_dir_path
 # backup copy with datetime
-for file in `find config_files -maxdepth 1 -type f -name ".*"`
-do
-    if [ -f $dot_dir_path ]
-    then
-        echo $file
-    fi
-done
+if [ $dotfiles_exists == TRUE ]
+then 
+    printf "Sync of config files\n"
+    for file in `find config_files -maxdepth 1 -type f -name ".*"`
+    do
+        file_name=`basename $file`
+
+        if [ `find $dot_dir_path -maxdepth 1 -type f -name $file_name -print -quit` ] 
+        then 
+            # file already exists 
+            # create a backup copy
+            printf "\tCreate backup of ${file_name}\n"
+            pushd $dot_dir_path > /dev/null
+                mv $file_name ".backup_${file_name:1}"
+            popd > /dev/null
+        fi
+
+        printf "\tCopy of ${file_name} in ${dot_dir_path}\n"
+        cp $file $dot_dir_path
+
+    done
+fi
+
+
